ed56ace421f86d748da9782723b4cba6add84f44
yeshiqi <1489368019@qq.com>
2021-06-24 15:31:00 +0800

    6-24-5 mybatis使用示例：开发/test/list接口，查询test表所有数据

--- src/main/java/com/yeshiqi/wiki/WikiApplication.java
+++ src/main/java/com/yeshiqi/wiki/WikiApplication.java
@@ -1,9 +1,11 @@
 package com.yeshiqi.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yeshiqi.wiki.mapper")
 public class WikiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/yeshiqi/wiki/controller/TestController.java
+++ src/main/java/com/yeshiqi/wiki/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.yeshiqi.wiki.controller;
 
+import com.yeshiqi.wiki.domain.Test;
+import com.yeshiqi.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
     @Value("${test.hello}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -18,5 +26,10 @@ public class TestController {
     @PostMapping ("/hello/post")
     public String helloPost(String name) {
         return "hello,world,post" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
