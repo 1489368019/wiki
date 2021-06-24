a49698410e43ef0d956f2c8c2e1a5e68793f0954
yeshiqi <1489368019@qq.com>
2021-06-24 20:03:48 +0800

    6-24-11 封装请求参数和返回参数

--- src/main/java/com/yeshiqi/wiki/service/EbookService.java
+++ src/main/java/com/yeshiqi/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.yeshiqi.wiki.service;
 
 import com.yeshiqi.wiki.domain.Ebook;
+import com.yeshiqi.wiki.domain.EbookExample;
 import com.yeshiqi.wiki.mapper.EbookMapper;
+import com.yeshiqi.wiki.req.EbookReq;
+import com.yeshiqi.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,7 +19,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
