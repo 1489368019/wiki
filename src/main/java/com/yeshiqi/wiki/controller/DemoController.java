4b95d5a70efa464aa50d449751defdf93f329cf0
yeshiqi <1489368019@qq.com>
2021-06-24 16:35:29 +0800

    6-24-7 增加demo表查询

---
+++ src/main/java/com/yeshiqi/wiki/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.yeshiqi.wiki.controller;
+
+import com.yeshiqi.wiki.domain.Demo;
+import com.yeshiqi.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
+}
