5c646e137af8a4cddd34d4fb8becd4969798490e
yeshiqi <1489368019@qq.com>
2021-06-24 11:07:25 +0800

    6-24-1 增加hello接口测试test.http

--- src/main/java/com/yeshiqi/wiki/controller/TestController.java
+++ src/main/java/com/yeshiqi/wiki/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.yeshiqi.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +9,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "hello,world";
+    }
+
+    @PostMapping ("/hello/post")
+    public String helloPost(String name) {
+        return "hello,world,post" + name;
     }
 }
