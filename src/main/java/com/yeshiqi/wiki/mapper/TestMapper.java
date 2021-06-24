f4e1f09897c91f08ab527f9738e20b19497f8cc2
yeshiqi <1489368019@qq.com>
2021-06-24 16:23:29 +0800

    6-24-6 集成mybatis generator，生成demo表代码

--- src/main/java/com/yeshiqi/wiki/mapper/TestMapper.java
+++ src/main/java/com/yeshiqi/wiki/mapper/TestMapper.java
@@ -1,9 +1,11 @@
 package com.yeshiqi.wiki.mapper;
 
 import com.yeshiqi.wiki.domain.Test;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface TestMapper {
 
     List<Test> list();
--- src/main/java/com/yeshiqi/wiki/service/TestService.java
+++ src/main/java/com/yeshiqi/wiki/service/TestService.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Service
 public class TestService {
 
+    //@Autowired
     @Resource
     private TestMapper testMapper;
 
