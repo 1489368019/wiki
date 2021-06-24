c2fca976e2d8b28bc8e064e597148d7ebf87a92c
yeshiqi <1489368019@qq.com>
2021-06-24 20:31:34 +0800

    6-24-12 制作CopyUtils封装BeanUtils

--- src/main/java/com/yeshiqi/wiki/service/EbookService.java
+++ src/main/java/com/yeshiqi/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.yeshiqi.wiki.domain.EbookExample;
 import com.yeshiqi.wiki.mapper.EbookMapper;
 import com.yeshiqi.wiki.req.EbookReq;
 import com.yeshiqi.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.yeshiqi.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,12 +24,14 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
+        /*List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
             EbookResp ebookResp = new EbookResp();
             BeanUtils.copyProperties(ebook,ebookResp);
             respList.add(ebookResp);
-        }
+        }*/
+
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
 
         return respList;
     }
